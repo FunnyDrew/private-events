1b2339c4f336178d3bf2e0da86d6820703699658
Rybin Andrey <rybin.ag@gmail.com>
2021-11-09 16:39:36 +0300

    adding login

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'events/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "events#index"
+  get "session/login", to: "session#login"
+  get "session/sign_up", to: "session#sign_up"
+  
 end
